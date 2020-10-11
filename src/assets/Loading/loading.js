f966f3b375f08d0b2303b21afb9072cea5a9ed88
fenix-react <mhmd.hsny.1992@gmail.com>
2020-10-11 15:01:31 +0330

    firstRelease

---
+++ src/assets/Loading/loading.js
@@ -0,0 +1,56 @@
+import React, {Component} from 'react'
+import './loading.css'
+
+class Loading extends Component {
+
+    render() {
+
+        return (
+            <div className='stage'>
+            <div className='layer'></div>
+            <div className='layer'></div>
+            <div className='layer'></div>
+            <div className='layer'></div>
+            <div className='layer'></div>
+            <div className='layer'></div>
+            <div className='layer'></div>
+            <div className='layer'></div>
+            <div className='layer'></div>
+            <div className='layer'></div>
+            <div className='layer'></div>
+            <div className='layer'></div>
+            <div className='layer'></div>
+            <div className='layer'></div>
+            <div className='layer'></div>
+            <div className='layer'></div>
+            <div className='layer'></div>
+            <div className='layer'></div>
+            <div className='layer'></div>
+            <div className='layer'></div>
+            <div className='layer'></div>
+            <div className='layer'></div>
+            <div className='layer'></div>
+            <div className='layer'></div>
+            <div className='layer'></div>
+            <div className='layer'></div>
+            <div className='layer'></div>
+            <div className='layer'></div>
+            <div className='layer'></div>
+            <div className='layer'></div>
+            <div className='layer'></div>
+            <div className='layer'></div>
+            <div className='layer'></div>
+            <div className='layer'></div>
+            <div className='layer'></div>
+            <div className='layer'></div>
+            <div className='layer'></div>
+            <div className='layer'></div>
+            <div className='layer'></div>
+            <div className='layer'></div>
+          </div>
+          
+        )
+    }
+}
+
+export default Loading
